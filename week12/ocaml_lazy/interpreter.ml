87a3be784f4d654f2477610cda527b4dabba86e4
SeoyeonKang <sykang2612@gmail.com>
2021-05-21 13:36:27 +0900

    upload skeleton

---
+++ week12/ocaml_lazy/interpreter.ml
@@ -0,0 +1,10 @@
+module F = Format
+
+(* practice & homework *)
+let rec interp_e (s : Store.t) (e : Ast.expr) : Store.value = 
+  
+
+(* practice & homework *)
+let interp (p : Ast.fvae) : Store.value = 
+ 
+ 
