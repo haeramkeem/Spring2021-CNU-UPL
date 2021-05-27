9e13f6eb3bc5712aca91cd1636dc10eadff501d2
SeoyeonKang <sykang2612@gmail.com>
2021-05-27 16:01:55 +0900

    upload skeleton

---
+++ week13/practice/interpreter.ml
@@ -0,0 +1,13 @@
+module F = Format
+
+(* practice & homework *)
+let rec interp_e (e : Ast.expr) (s : Store.t) : Store.value = 
+  
+
+(* practice & homework *)
+let rec interp_s (stmt : Ast.stmt) (s : Store.t) : Store.t = 
+  
+
+(* practice & homework *)
+let interp (p : Ast.program) : Store.t = 
+  
