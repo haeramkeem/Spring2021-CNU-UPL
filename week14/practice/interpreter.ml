c39c7e85e0aed3177060f88fcdcc3573867801de
SeoyeonKang <sykang2612@gmail.com>
2021-06-03 15:34:35 +0900

    upload skeleton

---
+++ week14/practice/interpreter.ml
@@ -0,0 +1,11 @@
+module F = Format
+
+(* practice & homework *)
+let rec interp_e (e : Ast.expr) ((env, mem) : Env.t * Mem.t) : Mem.value = 
+ 
+(* practice & homework *)
+let rec interp_s (stmt : Ast.stmt) ((env, mem) : Env.t * Mem.t) : Env.t * Mem.t = 
+  
+(* practice & homework *)
+let interp (p : Ast.program) : Env.t * Mem.t = 
+ 
